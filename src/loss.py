ea8ede038f2b9bb830a6214fc27e6e4f89d678ad
toukal <raouftoukal3@gmail.com>
2023-04-14 03:01:01 +0200

    ajout des loss

--- src/loss.py
+++ src/loss.py
@@ -8,5 +8,43 @@ class MSELoss (Loss):
         return (y-yhat)**2
     
     def backward(self, y, yhat):
+        assert y.shape == yhat.shape
         return -2*(y-yhat)
+    
+
+class BCELoss(Loss):
+    
+    def forward(self, y, yhat):
+        assert y.shape == yhat.shape
+        yhat+=1e-9
+        yhat/=yhat.sum(axis=1)
+        return -(y*np.log(yhat)+(1-y)*np.log(1-yhat))
+
+    def backward(self, y, yhat):
+        assert y.shape == yhat.shape
+        return -y/yhat +(1-y)/(1-yhat)
+
+class CELoss(Loss):
+    
+    def forward(self, y, yhat):
+        assert y.shape == yhat.shape
+        return (-y*yhat).sum(axis=1)
+
+    def backward(self, y, yhat):
+        assert y.shape == yhat.shape
+        return -y
+    
+class LOGCELoss(Loss):
+    
+    def forward(self, y, yhat):
+        assert y.shape == yhat.shape
+        yhat+=1e-9
+        yhat/=yhat.sum(axis=1)
+        return (-y*np.log(yhat)).sum(axis=1)
+
+    def backward(self, y, yhat):
+        assert y.shape == yhat.shape
+        yhat+=1e-9
+        yhat/=yhat.sum(axis=1)
+        return -y/yhat
     
--- src/optim.py
+++ src/optim.py
@@ -1,3 +1,5 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 class Optim:
 
@@ -14,3 +16,12 @@ class Optim:
         self.net.update_parameters(self.eps)
 
         return self._loss.forward(batch_y,y_hat)
+    
+    def SGD(self,batch_x,batch_y,batch_size=0.8,iter=1000):
+        
+        d_y=batch_y.shape[0]
+        assert batch_x.shape[0]==d_y
+
+        XY=np.concatenate((batch_x,batch_y),axis=1)
+        np.random.shuffle(XY)
+        train_test_split(XY,)
